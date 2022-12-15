32b1de87599598f95e350528637788a933fb31ba
msh <83120007+johntanas@users.noreply.github.com>
2022-12-15 14:35:49 +0800

    Delete kattis/221122 directory

--- kattis/221122/kinds.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-public class kinds {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter writer = new PrintWriter(System.out);
-    }
-}
